8496c6bc20783da9b3d9dc995e38b4bacafe5e0b
Preetabh Awasthi <125733238+vishubbk@users.noreply.github.com>
2025-03-06 22:00:29 +0530

    Update AuthMiddleware.js

--- backend/middlewares/AuthMiddleware.js
+++ backend/middlewares/AuthMiddleware.js
@@ -3,12 +3,15 @@ const jwt = require("jsonwebtoken");
 const AuthMiddleware = (req, res, next) => {
   const token = req.cookies.token;
 
+
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+
     req.user = decoded;
     next();
   } catch (error) {
