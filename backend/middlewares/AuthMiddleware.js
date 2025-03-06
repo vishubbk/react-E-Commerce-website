f9d0f3509b51e2d97d13da51fee3c464a075f58e
Preetabh Awasthi <125733238+vishubbk@users.noreply.github.com>
2025-03-06 21:53:37 +0530

    Update AuthMiddleware.js

--- backend/middlewares/AuthMiddleware.js
+++ backend/middlewares/AuthMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
+const AuthMiddleware = (req, res, next) => {
   const token = req.cookies.token;
 
   if (!token) {
@@ -17,4 +17,4 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = authMiddleware;
+module.exports = AuthMiddleware;
