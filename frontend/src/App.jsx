88ac5ce87f4730326f2f89ee8aa736be8cbff3b1
vishubbk <vishubbkup@gmail.com>
2025-03-07 22:24:17 +0530

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/UserFolder/Home';
-import UserLogin from './pages/UserFolder/userLogin';
-import UserCreate from './pages/UserFolder/userCreate';
-import UserProfile from './pages/UserFolder/userProfile';
-import UserProfileEdit from './pages/UserFolder/userProfileEdit';
-import MyOrders from './pages/UserFolder/MyOrders';
-import ProductsDetails from './pages/UserFolder/productsDetails';
-import OrderSuccess from './pages/UserFolder/orderSuccessPage';
-import UserCart from './pages/UserFolder/getCartItems';
-import Buynow from './pages/UserFolder/buynowSummery';
-import Logout from './pages/UserFolder/Logout';
 
-import OwnerLogin from './pages/OwnerFolder/OwnerLogin';
-import OwnerCreate from './pages/OwnerFolder/OwnerCreate';
-import OwnerDashboard from './pages/OwnerFolder/OwnerDashboard';
-import OwnerAllItems from './pages/OwnerFolder/OwnerAllItems';
-import OwnerAddItems from './pages/OwnerFolder/OwnerAddItems';
-import OwnerProfile from './pages/OwnerFolder/OwnerProfile';
-import OwnerProfileEdit from './pages/OwnerFolder/OwnerProfileEdit';
-import OwnerAllOrders from './pages/OwnerFolder/OwnerAllOrdersShow';
-
-import RouteProtection from './pages/RouteProtection';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/UserFolder/Home'
+import UserLogin from './pages/UserFolder/userLogin'
+import UserCreate from './pages/UserFolder/userCreate'
+import UserProfile from './pages/UserFolder/userProfile'
+import UserProfileEdit from './pages/UserFolder/userProfileEdit'
+import MyOrders from './pages/UserFolder/MyOrders'
+import ProductsDetails from './pages/UserFolder/productsDetails'
+import OwnerLogin from './pages/OwnerFolder/OwnerLogin'
+import OwnerCreate from './pages/OwnerFolder/OwnerCreate'
+import OwnerDashboard from './pages/OwnerFolder/OwnerDashboard'
+import OwnerAllItems from './pages/OwnerFolder/OwnerAllItems'
+import OwnerAddItems from './pages/OwnerFolder/OwnerAddItems'
+import OwnerProfile from './pages/OwnerFolder/OwnerProfile'
+import RouteProtection from './pages/RouteProtection'
 
 const App = () => {
   return (
     <Routes>
-      {/* Home Route */}
-      <Route path="/" element={<Home />} />
-
       {/* User Routes */}
-      <Route path="/users/register" element={<UserCreate />} />
+      <Route path="/" element={<Home />} />
       <Route path="/users/login" element={<UserLogin />} />
+      <Route path="/users/register" element={<UserCreate />} />
+      <Route path="/users/Order" element={<MyOrders />} />
       <Route path="/users/profile" element={<UserProfile />} />
       <Route path="/users/profile/edit" element={<UserProfileEdit />} />
-      <Route path="/users/getCartItems" element={<UserCart />} />
-      <Route path="/users/buynow/:id" element={<Buynow />} />
-      <Route path="/users/orderSuccess/:id" element={<OrderSuccess />} />
-      <Route path="/users/logout" element={<Logout />} />
-      <Route path="/users/Order" element={<MyOrders />} />
+      <Route path="/products/:id" element={<ProductsDetails />} />
 
       {/* Owner Routes */}
-      <Route path="/owner/register" element={<OwnerCreate />} />
       <Route path="/owner/login" element={<OwnerLogin />} />
+      <Route path="/owner/register" element={<OwnerCreate />} />
       <Route path="/owner/dashboard" element={<OwnerDashboard />} />
       <Route path="/owner/all-items" element={<OwnerAllItems />} />
       <Route path="/owner/add-items" element={<OwnerAddItems />} />
       <Route path="/owner/profile" element={<OwnerProfile />} />
-      <Route path="/owner/edit-profile" element={<OwnerProfileEdit />} />
-      <Route path="/owner/all-orders" element={<OwnerAllOrders />} />
 
-      {/* Product Routes */}
-      <Route path="/products/:id" element={<ProductsDetails />} />
-
-      {/* Route Protection */}
+      {/* Protection Route */}
       <Route path="*" element={<RouteProtection />} />
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,64 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import ReactDOM from "react-dom/client";  // ✅ Use "react-dom/client" for React 18
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import UserCreate from "./pages/UserFolder/userCreate";
+import UserLogin from "./pages/UserFolder/userLogin";
+import UserProfile from "./pages/UserFolder/userProfile";
+import UserProfileEdit from "./pages/UserFolder/UserProfileEdit";
+import UserCart from "./pages/UserFolder/getCartItems";
+import Buynow from "./pages/UserFolder/buynowSummery";
+import MyOrders from './pages/UserFolder/myOrders'
+import Home from "./pages/UserFolder/Home";
+import ProductsDetails from "./pages/UserFolder/ProductsDetails";
+import OrderSuccess from "./pages/UserFolder/orderSuccessPage";
+import RouteProtection from "./pages/UserFolder/RouteProtection";
+import Logout from "./Logout";
+
+import OwnerCreate from "./pages/UserFolder/OwnerCreate";
+import OwnerLogin from "./pages/UserFolder/OwnerLogin";
+import OwnerDashboard from "./pages/OwnerFolder/OwnerDashboard";
+import OwnerAllItems from "./pages/OwnerFolder/AllItems";
+import OwnerAddItems from "./pages/OwnerFolder/AddItems";
+import OwnerProfile from "./pages/OwnerFolder/Profile";
+import OwnerProfileEdit from "./pages/OwnerFolder/EditProfile";
+import OwnerAllOrders from "./pages/OwnerFolder/AllOrdersShow";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      {/* Home Route */}
+      <Route path="/" element={<Home />} />
+
+      {/* User Routes */}
+      <Route path="/users/register" element={<UserCreate />} />  {/* User Registration */}
+      <Route path="/users/login" element={<UserLogin />} />  {/* User Login */}
+      <Route path="/users/profile" element={<UserProfile />} />  {/* User Profile */}
+      <Route path="/users/profile/edit" element={<UserProfileEdit />} />  {/* Edit User Profile */}
+      <Route path="/users/getCartItems" element={<UserCart />} />  {/* User Cart Page */}
+      <Route path="/users/buynow/:id" element={<Buynow />} />  {/* Buy Now Checkout */}
+      <Route path="/users/orderSuccess/:id" element={<OrderSuccess />} />  {/* Order Success Page */}
+      <Route path="/users/logout" element={<Logout />} />  {/* Logout */}
+      <Route path="/users/Order" element={<MyOrders />} />  {/* Order-Page */}
+
+      {/* Owner Routes */}
+      <Route path="/owner/register" element={<OwnerCreate />} />  {/* Owner Registration */}
+      <Route path="/owner/login" element={<OwnerLogin />} />  {/* Owner Login */}
+      <Route path="/owner/dashboard" element={<OwnerDashboard />} />  {/* Owner Dashboard */}
+      <Route path="/owner/All-Items" element={<OwnerAllItems />} />  {/* View All Items */}
+      <Route path="/owner/Add-Items" element={<OwnerAddItems />} />  {/* Add New Items */}
+      <Route path="/owner/Profile" element={<OwnerProfile />} />  {/* Owner Profile */}
+      <Route path="/owner/editProfile" element={<OwnerProfileEdit />} />  {/* Edit Owner Profile */}
+      <Route path="/owner/AllOrders" element={<OwnerAllOrders />} />  {/* Edit Owner Profile */}
+
+      {/* Product Routes */}
+      <Route path="/products/:id" element={<ProductsDetails />} />  {/* Product Details Page */}
+
+      {/* Route Protection - Handles unauthorized access */}
+      <Route path="*" element={<RouteProtection />} />  {/* Default route for unauthorized pages */}
+    </Routes>
   </BrowserRouter>
 );
