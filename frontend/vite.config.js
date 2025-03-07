d03e392645984f9569a610e0ead6000d19b002c1
vishubbk <vishubbkup@gmail.com>
2025-03-07 21:24:28 +0530

    #

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",  // ✅ Render Deployment Fix
-  build: {
-    outDir: "dist",  // ✅ Correct Output Directory for Deployment
-  },
+  plugins: [react(), tailwindcss()],
+  base: "/", // ✅ Ensure correct base path
 });
+
