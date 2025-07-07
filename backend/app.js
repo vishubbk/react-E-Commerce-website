8bf69e261efe26b3032be75292520e272185f034
vishubbk <vishubbkup@gmail.com>
2025-07-07 12:33:28 +0530

    #

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // 🔹 CORS Configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://biggest-shop-mart.onrender.com/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
