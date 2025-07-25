a1785c2db737c5a5c905edc68e5892e2ef80b79c
vishubbk <vishubbkup@gmail.com>
2025-07-25 23:37:10 +0530

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 import Buynow from './pages/UserFolder/buynowSummery.jsx';
