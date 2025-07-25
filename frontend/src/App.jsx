072e794c2770cd59f7785554025e33af1f051f73
vishubbk <vishubbkup@gmail.com>
2025-07-25 23:33:56 +0530

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+
 
 import Buynow from './pages/UserFolder/buynowSummery.jsx';
 import UserCart from './pages/UserFolder/getCartItems.jsx';
